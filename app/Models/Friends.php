cd17540b93013c3831f72a6402389c5ce4744131
muhammadabdurrohim574@gmail.com <muhammadabdurrohim574@gmail.com>
2021-11-16 23:50:05 +0700

    commit 7

--- app/Models/Friends.php
+++ app/Models/Friends.php
@@ -9,7 +9,9 @@ class Friends extends Model
 {
     use HasFactory;
 
-    protected $guarded = ['mana'];
+    protected $fillable = [
+        'nama', 'no_tlp', 'alamat',
+    ];
 
     public function groups()
     {
